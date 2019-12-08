b6e4178fe9721ecbec84d498a5a44c3e630b2d47
Wasif Jawad Hussain <44579117+wasif1508@users.noreply.github.com>
2019-12-08 16:59:17 +0530

    Add files via upload

---
+++ nn_method/nn_method/config.py
@@ -0,0 +1,16 @@
+max_letters = 20
+language_tags = {
+
+                'en':['actor', 'alcohol', 'cheque', 'cancer', 'chocolate', 'debate', 'hobby', 'melon', 'propaganda',
+                      'religion', 'violin', 'england', 'MediaWiki','Sachin Tendulkar','London'],
+
+               
+                'de': ['Deutsche_Sprache', 'Deutschland', 'Kommunistische_Partei_der_Sowjetunion', 'Wasser',
+                    'Festkörper', 'Seele', 'Geist', 'Dreifaltigkeit', 'Große', 'Christentum', 'Gott'],
+
+
+                'fr': ['Français', 'Langues_romanes', 'Charlemagne', 'Traité_de_Verdun', 'Louis_le_Pieux',
+                    'Son_(physique)', 'Zoologie', 'Intelligence_animale', 'Intelligence', 'Tautologie',
+                    'Pléonasme', 'Figure_de_style']
+
+                 }
---
+++ nn_method/nn_method/functions.py
@@ -0,0 +1,48 @@
+import re
+import wikipedia as wiki
+from unidecode import unidecode
+import config
+
+
+def generate_dictionary(tag, max_word_length):
+
+    wiki.set_lang(tag)
+    for topic in config.language_tags[tag]:
+        page = wiki.WikipediaPage(topic)
+        content = page.content
+        content = unidecode(content)
+        lst = process(content, max_word_length)
+
+    return lst
+
+def process(page_content, max_word_length):
+    words = re.sub(r'[^a-zA-Z ]', '', page_content)
+    lower = words.lower()
+    word_list = lower.split()
+    short_words = []
+    for word in word_list:
+        if len(word) <= max_word_length:
+            short_words.append(word)
+    return short_words
+
+def convert_dic_to_vector(dic, max_word_length):
+    new_list = []
+    for word in dic:
+        vec = ''
+        n = len(word)
+        for i in range(n):
+            current_letter = word[i]
+            ind = ord(current_letter)-97
+            placeholder = (str(0)*ind) + str(1) + str(0)*(25-ind)
+            vec = vec + placeholder
+        if n < max_word_length:
+            excess = max_word_length-n
+            vec = vec +str(0)*26*excess
+        new_list.append(vec)
+    #print(len(new_list))
+    return new_list
+
+
+def create_output_vector(tag_index, number_of_languages):
+    out = str(0)*tag_index + str(1) + str(0)*(number_of_languages-1-tag_index)
+    return out
